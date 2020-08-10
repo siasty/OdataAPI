2973aace70667090ed69d332024e6753fde7e361
Maciej Miskiewicz <siasty3@gmail.com>
2020-08-10 15:51:11 +0200

    start odata

---
+++ OdataAPI/Data/TestDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdataAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdataAPI.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext()
+        {
+
+        }
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("Test");
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<ProductRating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
